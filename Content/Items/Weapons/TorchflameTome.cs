dcaee66760fd40ac888c01770ef6660adc44344b
Blu3 <124432839+harlowo@users.noreply.github.com>
2024-02-18 17:04:24 +0000

    Add files via upload

---
+++ Content/Items/Weapons/TorchflameTome.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace MoreTomes.Content.Items.Weapons
+{
+    public class TorchflameTome : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.damage = 5;
+            Item.DamageType = DamageClass.Magic;
+            Item.width = 28;
+            Item.height = 30;
+            Item.scale = 1f;
+            Item.shoot = ProjectileID.WandOfSparkingSpark;
+            Item.shootSpeed = 12f;
+            Item.mana = 6;
+            Item.useTime = 20;
+            Item.useAnimation = 20;
+            Item.useStyle = ItemUseStyleID.Shoot;
+            Item.autoReuse = true;
+        }
+        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+        {
+            int numProj = 2;
+            float rotation = MathHelper.ToRadians(2);
+            for (int i = 0; i < numProj; i++)
+            {
+                Vector2 perturbedSpeed = velocity.RotatedBy(MathHelper.Lerp(-rotation, rotation, i / (float)(numProj - 1)));
+                int proj = Projectile.NewProjectile(source, position, perturbedSpeed, type, damage, knockback, player.whoAmI);
+            }
+            return false;
+
+        }
+    }
+}
